9c6aa78a52dce7f282101e284693da4edb3de0c7
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-08-22 01:59:32 +0200

    Update new postcard logo animation

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -18,46 +18,25 @@ export default function Header() {
       <div className={"flex gap-14 items-center"}>
         <div
           className={
-            "group flex items-center gap-2 bg-emerald-500 rounded-3xl px-5 py-2.5 text-white hover:bg-blue-500 transition-colors cursor-pointer duration-300"
+            "group flex items-center gap-1 bg-emerald-500 rounded-3xl px-5 py-2.5 text-white hover:bg-blue-500 transition-colors cursor-pointer duration-300"
           }
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            strokeWidth={1.5}
+            strokeWidth="1.5"
             stroke="currentColor"
-            className="w-4 h-4 group-hover:hidden inline"
+            className="w-4 h-4 group-hover:animate-spin group-hover:animate-once group-hover:animate-ease-in-out"
           >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              d="M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H6.911a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661z"
+              d="M12 6v12m6-6H6"
             />
           </svg>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-4 h-4 group-hover:inline hidden"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9 3.75H6.912a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859M12 3v8.25m0 0l-3-3m3 3l3-3"
-            />
-          </svg>
-
           <span>New Postcard</span>
         </div>
-        {/*<div className={"flex gap-4 text-white"}>*/}
-        {/*  <div className={"bg-black rounded-3xl px-5 py-2.5"}>X</div>*/}
-        {/*  <div className={"bg-black rounded-3xl px-5 py-2.5"}>Fb</div>*/}
-        {/*  <div className={"bg-black rounded-3xl px-5 py-2.5"}>X</div>*/}
-        {/*</div>*/}
       </div>
     </div>
   );
