73417fd2c8f59536a17ee75cfdf679fd2cb769b5
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-08-25 14:55:01 +0200

    Update mt on footer

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className={"pb-4 flex flex-col items-center gap-1 pt-6 text-sm"}>
+    <div className={"pb-4 flex flex-col items-center gap-1 text-sm"}>
       <Link href={"https://www.bacqueyrisses.dev/"} target={"_blank"}>
         ✦ Made with love by Enzo ✦
       </Link>
