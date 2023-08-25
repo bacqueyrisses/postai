eb7648ba540585810977620540666b925279dcd6
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-08-25 18:55:26 +0200

    Update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,11 +18,6 @@ export default function RootLayout({
       <body
         className={`px-4 md:px-24 flex flex-col justify-between h-screen bg-[#E9E7DF] ${raleway.className}`}
       >
-        <link
-          rel="icon"
-          href="https://em-content.zobj.net/source/apple/354/world-map_1f5fa-fe0f.png"
-          sizes="any"
-        />
         {children}
       </body>
     </html>
