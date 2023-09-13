88b7da3f5f3ed723325dc15dca3f6e31389b958c
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-09-13 02:02:44 +0200

    Update generate bow shadow

--- components/Header.tsx
+++ components/Header.tsx
@@ -36,21 +36,6 @@ export default function Header() {
           "w-36 md:w-44 group inline-flex justify-center items-center gap-1.5 bg-emerald-500 rounded-3xl py-2 md:py-2.5 text-white hover:bg-blue-500 transition-colors cursor-pointer duration-300 text-sm md:text-base"
         }
       >
-        {/*<svg*/}
-        {/*  xmlns="http://www.w3.org/2000/svg"*/}
-        {/*  fill="none"*/}
-        {/*  viewBox="0 0 24 24"*/}
-        {/*  strokeWidth="1.5"*/}
-        {/*  stroke="currentColor"*/}
-        {/*  className="w-4 h-4 md:w-5 md:h-5 group-hover:animate-spin group-hover:animate-once group-hover:animate-ease-in-out group-hover:animate-duration-[1200ms]"*/}
-        {/*>*/}
-        {/*  <path*/}
-        {/*    strokeLinecap="round"*/}
-        {/*    strokeLinejoin="round"*/}
-        {/*    d="M12 6v12m6-6H6"*/}
-        {/*  />*/}
-        {/*</svg>*/}
-
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -62,14 +47,11 @@ export default function Header() {
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+            d="M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H6.911a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661z"
           />
         </svg>
 
         <span className={"w-24 md:w-28 -mr-3"}>My postcards</span>
-        {/*<span className={"w-24 md:w-28 text-start -mr-1"}>*/}
-        {/*  {pageDisplayed === "locations" ? "New Postcard" : "New Location"}*/}
-        {/*</span>*/}
       </Link>
     </div>
   );
