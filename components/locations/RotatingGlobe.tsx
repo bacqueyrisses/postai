ff86d40efcd1bc2aed381eea65a9df4065b569c6
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-09-20 11:12:27 +0200

    Update to NextJS 13.5 & update RortatingGlobe to work

--- components/locations/RotatingGlobe.tsx
+++ components/locations/RotatingGlobe.tsx
@@ -44,87 +44,117 @@ export default function RotatingGlobe({
     }
   }
 
+  function selectCountry(id: string, currentPolygon: any, currentChart: any) {
+    let dataItem = currentPolygon.getDataItemById(id);
+    let target = dataItem?.get("mapPolygon");
+    target?.set("active", true);
+
+    if (!target) return;
+
+    let centroid = target.geoCentroid();
+    if (centroid) {
+      currentChart.animate({
+        key: "rotationX",
+        to: -centroid.longitude,
+        duration: 1500,
+        easing: am5.ease.inOut(am5.ease.cubic),
+      });
+      currentChart.animate({
+        key: "rotationY",
+        to: -centroid.latitude,
+        duration: 1500,
+        easing: am5.ease.inOut(am5.ease.cubic),
+      });
+    }
+  }
+
   useEffect(() => {
-    // Create root element
-    const root = am5.Root.new("chartdiv", {
-      useSafeResolution: false,
-    });
-    root._logo?.dispose();
+    let root;
+    let chart;
+    let polygonSeries;
+    if (!rootRef.current) {
+      // Create root element
+      root = am5.Root.new("chartdiv", {
+        useSafeResolution: false,
+      });
+      root._logo?.dispose();
 
-    // Set themes
-    root.setThemes([am5themes_Animated.new(root), am5themes_Kelly.new(root)]);
-    rootRef.current = root;
+      // Set themes
+      root.setThemes([am5themes_Animated.new(root), am5themes_Kelly.new(root)]);
 
-    // Create the map chart
-    const chart = root.container.children.push(
-      am5map.MapChart.new(root, {
-        // panX: "rotateX", Add user manual rotation
-        // panY: "rotateY", Add user manual rotation
-        projection: am5map.geoOrthographic(),
-        maxZoomLevel: 1,
-        zoomLevel: 1,
-        panX: "none",
-        panY: "none",
-      }),
-    );
+      // Create the map chart
+      chart = root.container.children.push(
+        am5map.MapChart.new(root, {
+          // panX: "rotateX", Add user manual rotation
+          // panY: "rotateY", Add user manual rotation
+          projection: am5map.geoOrthographic(),
+          maxZoomLevel: 1,
+          zoomLevel: 1,
+          panX: "none",
+          panY: "none",
+        }),
+      );
 
-    // Create main polygon series for countries
-    const polygonSeries = chart.series.push(
-      am5map.MapPolygonSeries.new(root, {
-        geoJSON: am4geodata_worldLow,
-      }),
-    );
+      // Create main polygon series for countries
+      polygonSeries = chart.series.push(
+        am5map.MapPolygonSeries.new(root, {
+          geoJSON: am4geodata_worldLow,
+        }),
+      );
 
-    polygonSeries.mapPolygons.template.states.create("hover", {
-      fill: root.interfaceColors.get("primaryButtonHover"),
-    });
+      polygonSeries.mapPolygons.template.states.create("hover", {
+        fill: root.interfaceColors.get("primaryButtonHover"),
+      });
 
-    polygonSeries.mapPolygons.template.states.create("active", {
-      fill: am5.color("#047857"),
-    });
+      polygonSeries.mapPolygons.template.states.create("active", {
+        fill: am5.color("#047857"),
+      });
 
-    // Create series for background fill
-    const backgroundSeries = chart.series.push(
-      am5map.MapPolygonSeries.new(root, {}),
-    );
-    backgroundSeries.mapPolygons.template.setAll({
-      fill: root.interfaceColors.get("alternativeBackground"),
-      fillOpacity: 0.05,
-      strokeOpacity: 0,
-    });
-    polygonSeries.mapPolygons.template.setAll({
-      fill: am5.color("#17A34A"),
-      fillOpacity: 1,
-      // toggleKey: "active", Add user manual selection
-    });
+      // Create series for background fill
+      const backgroundSeries = chart.series.push(
+        am5map.MapPolygonSeries.new(root, {}),
+      );
+      backgroundSeries.mapPolygons.template.setAll({
+        fill: root.interfaceColors.get("alternativeBackground"),
+        fillOpacity: 0.05,
+        strokeOpacity: 0,
+      });
+      polygonSeries.mapPolygons.template.setAll({
+        fill: am5.color("#17A34A"),
+        fillOpacity: 1,
+        // toggleKey: "active", Add user manual selection
+      });
 
-    backgroundSeries.data.push({
-      geometry: am5map.getGeoRectangle(90, 180, -90, -180),
-    });
+      backgroundSeries.data.push({
+        geometry: am5map.getGeoRectangle(90, 180, -90, -180),
+      });
 
-    // Set up events
-    let previousPolygon: am5map.MapPolygon | null;
-    polygonSeries.mapPolygons.template.on("active", function (active, target) {
-      if (previousPolygon && previousPolygon !== target) {
-        previousPolygon.set("active", false);
-      }
-      if (target?.get("active")) {
-        selectCountry(target.dataItem.get("id"), polygonSeries, chart);
-      }
-      previousPolygon = target;
-    });
+      // Set up events
+      let previousPolygon: am5map.MapPolygon | null;
+      polygonSeries.mapPolygons.template.on(
+        "active",
+        function (active, target) {
+          if (previousPolygon && previousPolygon !== target) {
+            previousPolygon.set("active", false);
+          }
+          if (target?.get("active")) {
+            selectCountry(target.dataItem.get("id"), polygonSeries, chart);
+          }
+          previousPolygon = target;
+        },
+      );
 
-    // Animate on load
-    polygonSeries.events.on("datavalidated", function () {
-      selectCountry("US", polygonSeries, chart);
-    });
-
-    chartRef.current = chart;
-    polygonSeriesRef.current = polygonSeries;
+      // Animate on load
+      polygonSeries.events.on("datavalidated", function () {
+        selectCountry("US", polygonSeries, chart);
+      });
+      rootRef.current = root;
+      chartRef.current = chart;
+      polygonSeriesRef.current = polygonSeries;
+    }
   }, []);
 
   useEffect(() => {
-    console.log();
     // Animate on value change
     selectedCity.countryCode &&
       selectCountry(
