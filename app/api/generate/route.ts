87c091a5070fa213805205cd6bbd1a83a8dfdbd2
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-12-04 21:29:39 +0900

    Update prompt

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
 
   const city = searchParams.get("city");
-  const prompt = `a postcard of ${city}`;
+  const prompt = `a great and unique postcard of ${city}, I want you to make a cliché postcard of that city.`;
 
   try {
     const output: any = await replicate.run(model, { input: { prompt } });
