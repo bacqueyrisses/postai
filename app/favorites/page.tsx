25f8a8f996ddfd4eb5f591ef53278659340d558b
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-12-04 15:51:15 +0900

    Move fetch from API to component

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,10 +1,8 @@
 import { currentUser } from "@clerk/nextjs";
 import type { User } from "@clerk/nextjs/api";
 import FavoritePostcard from "@/components/containers/FavoritePostcard";
-
+import { sql } from "@vercel/postgres";
 import { unstable_noStore as noStore } from "next/cache";
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
 
 type Favorites = {
   id: number;
@@ -17,12 +15,10 @@ type Favorites = {
 async function fetchFavorites(user: User) {
   noStore();
   try {
-    const response = await fetch(
-      `${process.env.NEXT_SERVER_URL}/api/user/favorite/select-all?userId=${user?.id}`,
-      { next: { revalidate: 0 }, cache: "no-store" },
-    );
-    const data: Favorites[] = await response.json();
-    return data;
+    const data =
+      await sql<Favorites>`SELECT * FROM "Favorite" WHERE "userId" = ${user.id} ORDER BY "id" DESC;`;
+
+    return data.rows;
   } catch (error) {
     console.error("Database Error:", error);
     throw new Error("Failed to fetch favorites data.");
