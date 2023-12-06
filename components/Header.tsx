d8ca56b79b5df0728148d5b2d2e6017bcbb80645
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-12-06 17:46:53 +0900

    Update favorites link width

--- components/Header.tsx
+++ components/Header.tsx
@@ -31,7 +31,7 @@ export default function Header() {
         <Link
           href={"/favorites"}
           className={
-            "w-36 md:w-44 group inline-flex justify-center items-center gap-1.5 bg-emerald-500 rounded-3xl py-2 md:py-2.5 text-white hover:bg-blue-500 transition-colors cursor-pointer duration-300 text-sm md:text-base"
+            "w-32 md:w-44 group inline-flex justify-center items-center gap-1.5 bg-emerald-500 rounded-3xl py-2 md:py-2.5 text-white hover:bg-blue-500 transition-colors cursor-pointer duration-300 text-sm md:text-base"
           }
         >
           <span className={"w-24 md:w-32 text-center md:text-lg"}>
