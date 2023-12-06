dbb8d1ab973fbf2c79aacbbf51d1cf96e26eef55
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-12-06 17:44:05 +0900

    Update spacing of user avatar

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,7 +23,7 @@ export default function Header() {
         <div>postai</div>
       </Link>
 
-      <div className={"flex items-center sm:gap-4 gap-3"}>
+      <div className={"flex items-center sm:gap-4 gap-2.5"}>
         <UserButton
           appearance={{ elements: { avatarBox: "sm:w-8 sm:h-8 w-7 h-7" } }}
           afterSignOutUrl="/"
