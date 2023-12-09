9f9f91ab3561801783adfddb3a1b2c619ee57237
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-12-09 16:49:12 +0900

    Update generation strategy

--- components/containers/PostcardContainerWrapper.tsx
+++ components/containers/PostcardContainerWrapper.tsx
@@ -3,6 +3,7 @@ import EmailLinkButton from "@/components/buttons/EmailLinkButton";
 import PostcardContainer from "@/components/containers/PostcardContainer";
 import SaveButtons from "@/components/buttons/SaveButtons";
 import { unstable_noStore as noStore } from "next/cache";
+import { model, replicate } from "@/lib/replicate";
 
 export const revalidate = 0;
 
@@ -15,31 +16,33 @@ export default async function PostcardContainerWrapper({
   countryCode,
 }: IPostcardContainerWrapper) {
   const fetchGeneratedPostcard = async () => {
+    "use server";
     noStore();
-    // const prompt = `In the style of HISGH. Create a single vibrant, picturesque postcard image that captures the essence of ${city}. Incorporate iconic landmarks, the cityscape, or elements that symbolize its culture, history, and unique atmosphere. Emphasize vivid colors, bustling streets, and a lively ambiance to evoke a sense of wonder and excitement for anyone receiving this postcard.`;
-    // const width = 1024;
-    // const height = 768;
-    //
-    // try {
-    //   const output: any = await replicate.run(model, {
-    //     input: { prompt, height, width },
-    //   });
-    //
-    //   return output[0];
-    // } catch (error) {
-    //   console.log(error);
-    // }
-    const apiUrl = `/api/generate?city=${encodeURIComponent(city!)}`;
+
+    const prompt = `In the style of HISGH. Create a single vibrant, picturesque postcard image that captures the essence of ${city}. Incorporate iconic landmarks, the cityscape, or elements that symbolize its culture, history, and unique atmosphere. Emphasize vivid colors, bustling streets, and a lively ambiance to evoke a sense of wonder and excitement for anyone receiving this postcard.`;
+    const width = 1024;
+    const height = 768;
 
     try {
-      const response = await fetch(`http://localhost:3000${apiUrl}`);
-      const output: string = await response.json();
-      return output;
+      const output: any = await replicate.run(model, {
+        input: { prompt, height, width },
+      });
+      return output[0];
     } catch (error) {
-      console.error("Database Error:", error);
-      throw new Error("Failed to fetch data.");
+      console.log(error);
     }
   };
+  //   const apiUrl = `/api/generate?city=${encodeURIComponent(city!)}`;
+  //
+  //   try {
+  //     const response = await fetch(`http://localhost:3000${apiUrl}`);
+  //     const output: string = await response.json();
+  //     return output;
+  //   } catch (error) {
+  //     console.error("Database Error:", error);
+  //     throw new Error("Failed to fetch data.");
+  //   }
+  // };
 
   const favoriteUrl = await fetchGeneratedPostcard();
 
