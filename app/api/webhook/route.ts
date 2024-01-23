0af918f7078c57ff0f26ecf8eb985749d3e87590
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-01-23 17:14:38 +0900

    Remve secret check

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -6,13 +6,13 @@ export async function POST(req: Request) {
   const searchParams = new URL(req.url).searchParams;
   const id = searchParams.get("id") as string;
 
-  if (process.env.REPLICATE_WEBHOOK_SECRET) {
-    // if a secret is set, verify it
-    const secret = searchParams.get("secret") as string;
-    if (secret !== process.env.REPLICATE_WEBHOOK_SECRET) {
-      return new Response("Invalid secret", { status: 401 });
-    }
-  }
+  // if (process.env.REPLICATE_WEBHOOK_SECRET) {
+  //   // if a secret is set, verify it
+  //   const secret = searchParams.get("secret") as string;
+  //   if (secret !== process.env.REPLICATE_WEBHOOK_SECRET) {
+  //     return new Response("Invalid secret", { status: 401 });
+  //   }
+  // }
 
   // get output from Replicate
   const body = await req.json();
