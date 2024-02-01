5531eb765f7eff1a37a3e2d6f1a8c96edffccdbb
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-02-01 19:55:59 +0900

    Update webhook getter env

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const WEBHOOK_URL =
-  process.env.NEXT_PUBLIC_VERCEL_ENV === "development"
+  process.env.VERCEL_ENV === "development"
     ? `${process.env.NGROK_URL}/api/webhook`
     : `${process.env.NEXT_PUBLIC_URL}/api/webhook`;
