c917495e55f6713331ad1e821aa79cce3c007816
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-02-04 15:27:03 +0900

    Update w-h on loading circle

--- components/buttons/delete-button.tsx
+++ components/buttons/delete-button.tsx
@@ -35,7 +35,7 @@ function SubmitButton() {
   return (
     <button>
       {pending ? (
-        <LoadingCircle className={"h-2 w-2 md:h-4 md:w-4 text-white"} />
+        <LoadingCircle className={"h-3 w-3 md:h-4 md:w-4 text-white"} />
       ) : (
         <XCircle className="h-4 w-4 md:h-5 md:w-5 text-white" />
       )}
