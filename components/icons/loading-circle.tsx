4b7454a939ead74fc429a2c1c67d545ea52a6181
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-02-04 13:16:26 +0900

    Remove country-code-emoji library and replace it with local function for better performance

--- components/icons/loading-circle.tsx
+++ components/icons/loading-circle.tsx
@@ -1,8 +1,4 @@
-export default function LoadingCircle({
-  className,
-}: {
-  className?: string | undefined;
-}) {
+export default function LoadingCircle({ className }: { className?: string }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
---
+++ lib/country-emoji.ts
@@ -0,0 +1,5 @@
+export function getFlagEmoji(countryCode: string) {
+  return [...countryCode.toUpperCase()]
+    .map((char) => String.fromCodePoint(127397 + char.charCodeAt(0)))
+    .reduce((a, b) => `${a}${b}`);
+}
